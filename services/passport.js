efd74bcf83b64db6be16eadd5f9b821877ba8bd5
MC Qu <qumingcheng1020@hotmail.com>
2017-12-06 19:25:21 -0700

    Add client for login logout

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // user exists, we do not want to create new user
-          done(null, existingUser);
-        } else {
-          // user does not exist, we want to create new user
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // user exists, we do not want to create new user
+        return done(null, existingUser);
+      }
+      // user does not exist, we want to create new user
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
