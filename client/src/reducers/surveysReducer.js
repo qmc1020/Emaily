9c2528923542ee351c1832dc6925bbdb80749edf
MC Qu <qumingcheng1020@hotmail.com>
2017-12-31 14:25:03 -0700

    Beta finish

--- client/src/reducers/surveysReducer.js
+++ client/src/reducers/surveysReducer.js
@@ -3,7 +3,7 @@ import { FETCH_SURVEYS } from '../actions/types';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_SURVEYS:
-      return action.payload;
+      return action.payload || false; // returns false when action.payload is ''
     default:
       return state;
   }
